9445e8cd3d8209026c408be9b504472df2aa6ce7
jimatjtan <jimatjtan@92c2518b-485e-4ca2-9c37-138eef3f838b>
2001-03-17 17:20:21 +0000

    imported from my source tree
    git-svn-id: https://winlirc.svn.sourceforge.net/svnroot/winlirc/branches/jim@3 92c2518b-485e-4ca2-9c37-138eef3f838b

---
+++ gen_ir/readme
@@ -0,0 +1,20 @@
+This is a simple Winamp plugin that allows you to use WinLIRC to control
+some of the basic Winamp functions.  To use it, put the gen_ir.dll file
+in your Winamp plugin directory (usually "c:\program files\winamp\plugins").
+Then, load Winamp, go to Preferences, Misc Plugins, select the WinLIRC
+plugin, and hit Configure to set it up.
+
+Note: When adding a button, the "button id" is the textual name that
+you gave the button in the WinLIRC configuration file.
+
+
+Please see
+
+        http://home.jtan.com/~jim/winlirc/
+
+for more details about WinLIRC and this program.
+
+
+
+
+- Jim Paris <jim@jtan.com>
---
+++ gen_ir/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by gen_ir.rc
+//
+#define IDAPPLY                         3
+#define IDP_SOCKETS_INIT_FAILED         101
+#define IDC_CONF                        1000
+#define IDD_CONFIG                      1001
+#define IDC_ADD                         1002
+#define IDC_DELETE                      1003
+#define IDD_ADD                         1003
+#define IDC_ID                          1004
+#define IDC_ACTION                      1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        1004
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           1000
+#endif
+#endif
