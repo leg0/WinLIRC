422fea0d6edd515cb30d66cc31b3167ee68790a5
baily <baily@92c2518b-485e-4ca2-9c37-138eef3f838b>
2002-03-24 22:42:55 +0000

    cosmetic changes
    git-svn-id: svn+ssh://svn.code.sf.net/p/winlirc/code/trunk@32 92c2518b-485e-4ca2-9c37-138eef3f838b

--- winlirc/version.h
+++ winlirc/version.h
@@ -1,6 +1,6 @@
 #ifndef WINLIRC_VERSION
 
-#define WINLIRC_VERSION "0.6.1pre2"
+#define WINLIRC_VERSION "0.6.1pre3"
 
 static char id[]="WinLIRC " WINLIRC_VERSION " by <jim@jtan.com> and <baily@uiuc.edu>";
 #endif
