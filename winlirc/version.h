d091853bc8bfe06ba5e7002c938e9cec1f72a803
baily <baily@92c2518b-485e-4ca2-9c37-138eef3f838b>
2002-07-13 01:22:41 +0000

    0.6.4 release preparation
    git-svn-id: svn+ssh://svn.code.sf.net/p/winlirc/code/trunk@47 92c2518b-485e-4ca2-9c37-138eef3f838b

--- winlirc/version.h
+++ winlirc/version.h
@@ -1,6 +1,6 @@
 #ifndef WINLIRC_VERSION
 
-#define WINLIRC_VERSION "0.6.3"
+#define WINLIRC_VERSION "0.6.4"
 
 static char id[]="WinLIRC " WINLIRC_VERSION " by <jim@jtan.com> and <baily@uiuc.edu>";	
 #endif
