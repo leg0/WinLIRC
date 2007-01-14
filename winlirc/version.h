eae39718f0939694705980a414711b12e9b20cb6
alexsaa <alexsaa@92c2518b-485e-4ca2-9c37-138eef3f838b>
2007-01-14 22:17:53 +0000

    (1) version 0.6.5a: force 'special repeat code' option; (2) 'et al.' added to author lists
    git-svn-id: svn+ssh://svn.code.sf.net/p/winlirc/code/trunk@59 92c2518b-485e-4ca2-9c37-138eef3f838b

--- winlirc/version.h
+++ winlirc/version.h
@@ -1,7 +1,8 @@
 #ifndef WINLIRC_VERSION
 
-#define WINLIRC_VERSION "0.6.5"
+#define WINLIRC_VERSION "0.6.5a"
 
-static char id[]="WinLIRC " WINLIRC_VERSION " by <jim@jtan.com> and <baily@uiuc.edu>";	
+static char id[]="WinLIRC " WINLIRC_VERSION " by <jim@jtan.com>, <baily@uiuc.edu> et al.";
+
 #endif
 
