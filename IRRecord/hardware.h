fa78b6c86e4300677f7089dcf7eda3c4e342e440
dukeeeey <dukeeeey@92c2518b-485e-4ca2-9c37-138eef3f838b>
2010-06-28 23:36:30 +0000

    Updates to support new receiver types. Cleaned code a little.
    git-svn-id: https://winlirc.svn.sourceforge.net/svnroot/winlirc/trunk@81 92c2518b-485e-4ca2-9c37-138eef3f838b

--- IRRecord/hardware.h
+++ IRRecord/hardware.h
@@ -1,3 +1,24 @@
+/* 
+ * This file is part of the WinLIRC package, which was derived from
+ * LIRC (Linux Infrared Remote Control) 0.8.6.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Copyright (C) 2010 Ian Curtis
+ */
+
 #ifndef HARDWARE_H
 #define HARDWARE_H
 
@@ -32,6 +53,7 @@ struct hardware
 	lirc_t	(*readdata)		(lirc_t timeout);
 	void	(*wait_for_data)(lirc_t timeout);
 	int		(*data_ready)	(void);
+	ir_code (*get_ir_code)	(void);
 };
 
 extern struct hardware hw;
