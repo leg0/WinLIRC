2a9185ca5d68d3f97b4ad0a93762a681ac1b3ce2
dukeeeey <dukeeeey@92c2518b-485e-4ca2-9c37-138eef3f838b>
2010-07-13 23:29:53 +0000

    Framework for a working plugin, just needs data to decode :)
    git-svn-id: https://winlirc.svn.sourceforge.net/svnroot/winlirc/trunk@87 92c2518b-485e-4ca2-9c37-138eef3f838b

---
+++ DLL/IRToy/Globals.cpp
@@ -0,0 +1,44 @@
+/* 
+ * This file is part of the WinLIRC package, which was derived from
+ * LIRC (Linux Infrared Remote Control) 0.8.6.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Copyright (C) 2010 Ian Curtis
+ */
+
+#include <Windows.h>
+#include "Globals.h"
+#include <stdio.h>
+#include "LircDefines.h"
+
+Settings settings;
+
+HANDLE	threadExitEvent	= NULL;
+HANDLE	dataReadyEvent	= NULL;
+
+SendReceiveData *sendReceiveData = NULL;
+
+int gettimeofday(struct mytimeval *a, void *)
+/* only accurate to milliseconds, instead of microseconds */
+{
+	struct _timeb tstruct;
+	_ftime(&tstruct);
+	
+	a->tv_sec=tstruct.time;
+	a->tv_usec=tstruct.millitm*1000;
+
+	return 1;
+}
---
+++ DLL/IRToy/IRToy.cpp
@@ -0,0 +1,179 @@
+/* 
+ * This file is part of the WinLIRC package, which was derived from
+ * LIRC (Linux Infrared Remote Control) 0.8.6.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * Copyright (C) 2010 Ian Curtis
+ */
+
+#include <Windows.h>
+#include "LIRCDefines.h"
+#include <stdio.h>
+#include "Globals.h"
+#include "hardware.h"
+#include "Decode.h"
+#include "IRToy.h"
+#include "resource.h"
+#include <tchar.h>
+
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+
+IG_API int init(HANDLE exitEvent) {
+
+	init_rec_buffer();
+	initHardwareStruct();
+
+	sendReceiveData = new SendReceiveData();
+
+	threadExitEvent = exitEvent;
+	dataReadyEvent	= CreateEvent(NULL,FALSE,FALSE,NULL);
+
+	return 1;
+}
+
+IG_API void deinit() {
+
+	if(dataReadyEvent) {
+		CloseHandle(dataReadyEvent);
+		dataReadyEvent = NULL;
+	}
+
+	if(sendReceiveData) {
+		sendReceiveData->deinit();
+		delete sendReceiveData;
+		sendReceiveData = NULL;
+	}
+
+	threadExitEvent = NULL;
+
+}
+
+IG_API int hasGui() {
+
+	return TRUE;
+}
+
+BOOL CALLBACK dialogProc (HWND hwnd, 
+                          UINT message, 
+                          WPARAM wParam, 
+                          LPARAM lParam) {
+
+    switch (message) {
+
+		case WM_INITDIALOG: {
+
+			//============
+			TCHAR temp[2];
+			//============
+
+			for(int i=1; i<10; i++) {
+
+				_stprintf(temp,_T("%i"),i);
+				SendDlgItemMessage(hwnd,IDC_COMBO1,CB_ADDSTRING,0,(LPARAM)temp);
+			}
+
+			SendDlgItemMessage(hwnd,IDC_COMBO1,CB_SETCURSEL,settings.getComPort()-1,0);
+
+			ShowWindow(hwnd, SW_SHOW);
+
+			return TRUE;
+		}
+
+		case WM_COMMAND: {
+
+			switch(LOWORD(wParam)) {
+
+				case IDOK: {
+
+					settings.setComPort(SendDlgItemMessage(hwnd,IDC_COMBO1,CB_GETCURSEL,0,0)+1);
+					settings.saveSettings();
+
+					DestroyWindow (hwnd);
+					return TRUE;
+				}
+
+				case IDCANCEL: {
+					//
+					//ignore changes
+					//
+					DestroyWindow (hwnd);
+					return TRUE;
+				}
+
+			}
+
+			return FALSE;
+
+		}
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return TRUE;
+		case WM_CLOSE:
+			DestroyWindow (hwnd);
+			return TRUE;
+	}
+
+    return FALSE;
+
+}
+
+IG_API void	loadSetupGui() {
+
+	//==============
+	HWND	hDialog;
+	MSG		msg;
+    INT		status;
+	//==============
+
+	hDialog = CreateDialog((HINSTANCE)(&__ImageBase),MAKEINTRESOURCE(IDD_DIALOG1),NULL,dialogProc);
+
+    while ((status = GetMessage (& msg, 0, 0, 0)) != 0) {
+
+        if (status == -1) return;
+
+        if (!IsDialogMessage (hDialog, & msg)) {
+
+            TranslateMessage ( & msg );
+            DispatchMessage ( & msg );
+        }
+    }
+
+}
+
+IG_API int sendIR(struct ir_remote *remote, struct ir_ncode *code, int repeats) {
+
+	return 0;
+}
+
+IG_API int decodeIR(struct ir_remote *remotes, char *out) {
+
+	if(sendReceiveData) {
+		sendReceiveData->waitTillDataIsReady(0);
+		
+		if(decodeCommand(remotes,out)) {
+			return 1;
+		}
+	}
+	
+
+	return 0;
+}
+
+IG_API struct hardware* getHardware() {
+
+	initHardwareStruct();
+	return &hw;
+}
