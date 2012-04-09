e9d78a9bda9b6020ceab5de8a0d191732c402e50
crazycat69 <crazycat69@92c2518b-485e-4ca2-9c37-138eef3f838b>
2012-04-09 15:41:45 +0000

    New RC picture in config dialog.
    git-svn-id: https://winlirc.svn.sourceforge.net/svnroot/winlirc/trunk@138 92c2518b-485e-4ca2-9c37-138eef3f838b

--- DLL/TbsNxp/resource.h
+++ DLL/TbsNxp/resource.h
@@ -6,7 +6,8 @@
 #define IDD_DIALOG_CFG                  101
 #define IDB_LOGO                        105
 #define IDB_RC                          106
-#define IDB_BITMAP1                     109
+#define IDB_BITMAP1                     110
+#define IDB_RCNEW                       110
 #define IDC_COMBO_DEVCAT                1005
 #define IDC_COMBO_DEVID                 1007
 #define IDC_COMBO1                      1008
@@ -16,7 +17,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_RESOURCE_VALUE        111
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
