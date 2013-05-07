74f05860b5921dc6e567e01ab4108b67895eb088
dukeeeey <dukeeeey@92c2518b-485e-4ca2-9c37-138eef3f838b>
2013-05-07 19:36:19 +0000

    bump version
    git-svn-id: svn+ssh://svn.code.sf.net/p/winlirc/code/trunk@173 92c2518b-485e-4ca2-9c37-138eef3f838b

--- winlirc/version.h
+++ winlirc/version.h
@@ -1,7 +1,7 @@
 #ifndef WINLIRC_VERSION
 
-#define WINLIRC_VERSION "0.9.0e"
+#define WINLIRC_VERSION "0.9.0f"
 
-static char id[]="WinLIRC " WINLIRC_VERSION " by <jim@jtan.com> and <baily@uiuc.edu>";	
+static char id[]="WinLIRC " WINLIRC_VERSION " by <jim@jtan.com>, <baily@uiuc.edu> and <i.curtis@gmail.com>";	
 #endif
 
