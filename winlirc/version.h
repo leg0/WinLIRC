5288304a23a600e53743cd18f7fd19235a40a273
dukeeeey <dukeeeey@92c2518b-485e-4ca2-9c37-138eef3f838b>
2013-12-10 17:14:11 +0000

    adding missing version change
    git-svn-id: svn+ssh://svn.code.sf.net/p/winlirc/code/trunk@237 92c2518b-485e-4ca2-9c37-138eef3f838b

--- winlirc/version.h
+++ winlirc/version.h
@@ -1,6 +1,6 @@
 #ifndef WINLIRC_VERSION
 
-#define WINLIRC_VERSION _T("0.9.0f")
+#define WINLIRC_VERSION _T("0.9.0g")
 
 static TCHAR id[]=_T("WinLIRC ") WINLIRC_VERSION _T(" by <jim@jtan.com>, <baily@uiuc.edu> and <i.curtis@gmail.com>");	
 #endif
