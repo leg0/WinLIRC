1d26d25b75e71f277c9ef61e04bfcc31c994c272
dukeeeey <dukeeeey@92c2518b-485e-4ca2-9c37-138eef3f838b>
2014-02-28 00:57:19 +0000

    more com stuff
    git-svn-id: svn+ssh://svn.code.sf.net/p/winlirc/code/trunk@288 92c2518b-485e-4ca2-9c37-138eef3f838b

--- DLL/TbsCxt/TbsCxtRC.cpp
+++ DLL/TbsCxt/TbsCxtRC.cpp
@@ -53,81 +53,85 @@ BOOL CALLBACK dialogProc (HWND hwnd,
 
 		case WM_INITDIALOG: {
 			CoInitializeEx(NULL,COINIT_MULTITHREADED);
-			int numberOfDevices=0;
-			SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_RESETCONTENT,0,0);
-
-			// create system device enumerator
-			CComPtr <ICreateDevEnum>	pSysDevEnum = NULL;	
-			HRESULT hr = pSysDevEnum.CoCreateInstance(CLSID_SystemDeviceEnum);
-			if (hr == S_OK)
 			{
-				// create a class enumerator for the desired category defined by classGuid.
-				CComPtr <IEnumMoniker> pEnumCat = NULL;	//moniker enumerator for filter categories
-				hr = pSysDevEnum->CreateClassEnumerator(CLSID_VideoInputDeviceCategory, &pEnumCat, 0);
+				int numberOfDevices=0;
+				SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_RESETCONTENT,0,0);
+
+				// create system device enumerator
+				CComPtr <ICreateDevEnum>	pSysDevEnum = NULL;	
+				HRESULT hr = pSysDevEnum.CoCreateInstance(CLSID_SystemDeviceEnum);
 				if (hr == S_OK)
 				{
-
-					// reset the enumeration
-					pEnumCat->Reset();
-
-					// now iterate through enumeration
-					ULONG cFetched = 0;
-					CComPtr <IMoniker> pMoniker = NULL;
-
-					// get a pointer to each moniker
-					while(pEnumCat->Next(1, &pMoniker, &cFetched) == S_OK)
+					// create a class enumerator for the desired category defined by classGuid.
+					CComPtr <IEnumMoniker> pEnumCat = NULL;	//moniker enumerator for filter categories
+					hr = pSysDevEnum->CreateClassEnumerator(CLSID_VideoInputDeviceCategory, &pEnumCat, 0);
+					if (hr == S_OK)
 					{
-						//get a pointer to property bag (which has filter)
-						IPropertyBag* pPropBag = NULL;	
-						if (pMoniker->BindToStorage(0, 0, IID_IPropertyBag, (void **)&pPropBag) != S_OK)
-							break;
 
-						TCHAR szFriendlyName[MAX_PATH];
+						// reset the enumeration
+						pEnumCat->Reset();
 
-						CComPtr <IBaseFilter> pFilter = NULL;
-						// create an instance of the filter
-						hr = pMoniker->BindToObject(0, 0, IID_IBaseFilter, (void**)&pFilter);
-						if (hr == S_OK)
-						{							
-							VARIANT varName;
-							// retrieve the friendly name of the filter
-							VariantInit(&varName);
-							hr = pPropBag->Read(L"FriendlyName", &varName, 0);
-							WideCharToMultiByte(CP_ACP, 0, varName.bstrVal, -1, szFriendlyName, sizeof(szFriendlyName), 0, 0);
-							VariantClear(&varName);
+						// now iterate through enumeration
+						ULONG cFetched = 0;
+						CComPtr <IMoniker> pMoniker = NULL;
 
-							CComPtr <IKsPropertySet> pKsVCPropSet = NULL;
-							// query for interface
-							hr = pFilter->QueryInterface(IID_IKsPropertySet, (void **)&pKsVCPropSet);
-							if (pKsVCPropSet)
-							{
-								DWORD type_support;
-								hr = pKsVCPropSet->QuerySupported(KSPROPSETID_CustomIRCaptureProperties,
-									KSPROPERTY_IRCAPTURE_COMMAND,
-									&type_support);
-								if ( SUCCEEDED(hr) && (type_support & KSPROPERTY_SUPPORT_SET) )
+						// get a pointer to each moniker
+						while(pEnumCat->Next(1, &pMoniker, &cFetched) == S_OK)
+						{
+							//get a pointer to property bag (which has filter)
+							IPropertyBag* pPropBag = NULL;	
+							if (pMoniker->BindToStorage(0, 0, IID_IPropertyBag, (void **)&pPropBag) != S_OK)
+								break;
+
+							TCHAR szFriendlyName[MAX_PATH];
+
+							CComPtr <IBaseFilter> pFilter = NULL;
+							// create an instance of the filter
+							hr = pMoniker->BindToObject(0, 0, IID_IBaseFilter, (void**)&pFilter);
+							if (hr == S_OK)
+							{							
+								VARIANT varName;
+								// retrieve the friendly name of the filter
+								VariantInit(&varName);
+								hr = pPropBag->Read(L"FriendlyName", &varName, 0);
+								WideCharToMultiByte(CP_ACP, 0, varName.bstrVal, -1, szFriendlyName, sizeof(szFriendlyName), 0, 0);
+								VariantClear(&varName);
+
+								CComPtr <IKsPropertySet> pKsVCPropSet = NULL;
+								// query for interface
+								hr = pFilter->QueryInterface(IID_IKsPropertySet, (void **)&pKsVCPropSet);
+								if (pKsVCPropSet)
 								{
-									PCHAR t=strstr(szFriendlyName,"Video Capture");
-									if (t) *t=0;
-									SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_ADDSTRING,0,(LPARAM)szFriendlyName);
-									numberOfDevices++;
+									DWORD type_support;
+									hr = pKsVCPropSet->QuerySupported(KSPROPSETID_CustomIRCaptureProperties,
+										KSPROPERTY_IRCAPTURE_COMMAND,
+										&type_support);
+									if ( SUCCEEDED(hr) && (type_support & KSPROPERTY_SUPPORT_SET) )
+									{
+										PCHAR t=strstr(szFriendlyName,"Video Capture");
+										if (t) *t=0;
+										SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_ADDSTRING,0,(LPARAM)szFriendlyName);
+										numberOfDevices++;
+									}
 								}
 							}
+							pMoniker.Release();
 						}
-						pMoniker.Release();
 					}
 				}
+			
+				if (settings.getDeviceNumber()>=numberOfDevices)
+					settings.setDeviceNumber(0);
+
+				if (numberOfDevices == 0)
+					SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_ADDSTRING,0,(LPARAM)"NO DEVICE");
+
+				SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_SETCURSEL,settings.getDeviceNumber(),0);
+
+				ShowWindow(hwnd, SW_SHOW);
 			}
-		
-			if (settings.getDeviceNumber()>=numberOfDevices)
-				settings.setDeviceNumber(0);
 
-			if (numberOfDevices == 0)
-				SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_ADDSTRING,0,(LPARAM)"NO DEVICE");
-
-			SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_SETCURSEL,settings.getDeviceNumber(),0);
-
-			ShowWindow(hwnd, SW_SHOW);
+			CoUninitialize ();
 
 			return TRUE;
 		}
--- DLL/TbsQBOX/TbsQboxRC.cpp
+++ DLL/TbsQBOX/TbsQboxRC.cpp
@@ -53,81 +53,85 @@ BOOL CALLBACK dialogProc (HWND hwnd,
 
 		case WM_INITDIALOG: {
 			CoInitializeEx(NULL,COINIT_MULTITHREADED);
-			int numberOfDevices=0;
-			SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_RESETCONTENT,0,0);
-
-			// create system device enumerator
-			CComPtr <ICreateDevEnum>	pSysDevEnum = NULL;	
-			HRESULT hr = pSysDevEnum.CoCreateInstance(CLSID_SystemDeviceEnum);
-			if (hr == S_OK)
 			{
-				// create a class enumerator for the desired category defined by classGuid.
-				CComPtr <IEnumMoniker> pEnumCat = NULL;	//moniker enumerator for filter categories
-				hr = pSysDevEnum->CreateClassEnumerator(KSCATEGORY_BDA_NETWORK_TUNER, &pEnumCat, 0);
+				int numberOfDevices=0;
+				SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_RESETCONTENT,0,0);
+
+				// create system device enumerator
+				CComPtr <ICreateDevEnum>	pSysDevEnum = NULL;	
+				HRESULT hr = pSysDevEnum.CoCreateInstance(CLSID_SystemDeviceEnum);
 				if (hr == S_OK)
 				{
-
-					// reset the enumeration
-					pEnumCat->Reset();
-
-					// now iterate through enumeration
-					ULONG cFetched = 0;
-					CComPtr <IMoniker> pMoniker = NULL;
-
-					// get a pointer to each moniker
-					while(pEnumCat->Next(1, &pMoniker, &cFetched) == S_OK)
+					// create a class enumerator for the desired category defined by classGuid.
+					CComPtr <IEnumMoniker> pEnumCat = NULL;	//moniker enumerator for filter categories
+					hr = pSysDevEnum->CreateClassEnumerator(KSCATEGORY_BDA_NETWORK_TUNER, &pEnumCat, 0);
+					if (hr == S_OK)
 					{
-						//get a pointer to property bag (which has filter)
-						IPropertyBag* pPropBag = NULL;	
-						if (pMoniker->BindToStorage(0, 0, IID_IPropertyBag, (void **)&pPropBag) != S_OK)
-							break;
 
-						TCHAR szFriendlyName[MAX_PATH];
+						// reset the enumeration
+						pEnumCat->Reset();
 
-						CComPtr <IBaseFilter> pFilter = NULL;
-						// create an instance of the filter
-						hr = pMoniker->BindToObject(0, 0, IID_IBaseFilter, (void**)&pFilter);
-						if (hr == S_OK)
-						{							
-							VARIANT varName;
-							// retrieve the friendly name of the filter
-							VariantInit(&varName);
-							hr = pPropBag->Read(L"FriendlyName", &varName, 0);
-							WideCharToMultiByte(CP_ACP, 0, varName.bstrVal, -1, szFriendlyName, sizeof(szFriendlyName), 0, 0);
-							VariantClear(&varName);
+						// now iterate through enumeration
+						ULONG cFetched = 0;
+						CComPtr <IMoniker> pMoniker = NULL;
 
-							CComPtr <IKsPropertySet> pKsTunerPropSet = NULL;
-							// query for interface
-							hr = pFilter->QueryInterface(IID_IKsPropertySet, (void **)&pKsTunerPropSet);
-							if (pKsTunerPropSet)
-							{
-								DWORD type_support;
-								hr = pKsTunerPropSet->QuerySupported(KSPROPERTYSET_QBOXControl,
-									KSPROPERTY_CTRL_IR,
-									&type_support);
-								if ( SUCCEEDED(hr) && (type_support & KSPROPERTY_SUPPORT_GET) )
+						// get a pointer to each moniker
+						while(pEnumCat->Next(1, &pMoniker, &cFetched) == S_OK)
+						{
+							//get a pointer to property bag (which has filter)
+							IPropertyBag* pPropBag = NULL;	
+							if (pMoniker->BindToStorage(0, 0, IID_IPropertyBag, (void **)&pPropBag) != S_OK)
+								break;
+
+							TCHAR szFriendlyName[MAX_PATH];
+
+							CComPtr <IBaseFilter> pFilter = NULL;
+							// create an instance of the filter
+							hr = pMoniker->BindToObject(0, 0, IID_IBaseFilter, (void**)&pFilter);
+							if (hr == S_OK)
+							{							
+								VARIANT varName;
+								// retrieve the friendly name of the filter
+								VariantInit(&varName);
+								hr = pPropBag->Read(L"FriendlyName", &varName, 0);
+								WideCharToMultiByte(CP_ACP, 0, varName.bstrVal, -1, szFriendlyName, sizeof(szFriendlyName), 0, 0);
+								VariantClear(&varName);
+
+								CComPtr <IKsPropertySet> pKsTunerPropSet = NULL;
+								// query for interface
+								hr = pFilter->QueryInterface(IID_IKsPropertySet, (void **)&pKsTunerPropSet);
+								if (pKsTunerPropSet)
 								{
-									PCHAR t=strstr(szFriendlyName,"Tuner");
-									if (t) *t=0;
-									SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_ADDSTRING,0,(LPARAM)szFriendlyName);
-									numberOfDevices++;
+									DWORD type_support;
+									hr = pKsTunerPropSet->QuerySupported(KSPROPERTYSET_QBOXControl,
+										KSPROPERTY_CTRL_IR,
+										&type_support);
+									if ( SUCCEEDED(hr) && (type_support & KSPROPERTY_SUPPORT_GET) )
+									{
+										PCHAR t=strstr(szFriendlyName,"Tuner");
+										if (t) *t=0;
+										SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_ADDSTRING,0,(LPARAM)szFriendlyName);
+										numberOfDevices++;
+									}
 								}
 							}
+							pMoniker.Release();
 						}
-						pMoniker.Release();
 					}
 				}
+			
+				if (settings.getDeviceNumber()>=numberOfDevices)
+					settings.setDeviceNumber(0);
+
+				if (numberOfDevices == 0)
+					SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_ADDSTRING,0,(LPARAM)"NO DEVICE");
+
+				SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_SETCURSEL,settings.getDeviceNumber(),0);
+
+				ShowWindow(hwnd, SW_SHOW);
 			}
-		
-			if (settings.getDeviceNumber()>=numberOfDevices)
-				settings.setDeviceNumber(0);
 
-			if (numberOfDevices == 0)
-				SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_ADDSTRING,0,(LPARAM)"NO DEVICE");
-
-			SendDlgItemMessage(hwnd,IDC_COMBO_DEVID,CB_SETCURSEL,settings.getDeviceNumber(),0);
-
-			ShowWindow(hwnd, SW_SHOW);
+			CoUninitialize ();
 
 			return TRUE;
 		}
