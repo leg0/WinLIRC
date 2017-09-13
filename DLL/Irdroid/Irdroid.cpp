26acdd64d1da3bd687e8e354ef8063314af8b95e
Hardware Group LTD <info@irdroid.com>
2017-09-13 16:14:04 +0300

    Support for Irdroid USB Infrared Transceiver
    Support for Irdroid USB Infrared Transceiver

--- DLL/Irdroid/Irdroid.cpp
+++ DLL/Irdroid/Irdroid.cpp
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
+ * Irdroid is a OSHW module based on DP IRToy
  * Copyright (C) 2010 Ian Curtis
  */
 
